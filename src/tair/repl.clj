e247d545947df8fa6bc0802c398b50d04ab0dbb3
James Xu <xumingming64398966@gmail.com>
2012-11-23 00:56:15 +0800

    cleanup & make it possible to change config at runtime

--- src/tair/repl.clj
+++ src/tair/repl.clj
@@ -8,25 +8,39 @@
                 (.setDynamicConfig true))]
     tair))
 
-(def *namespace* 99)
-(def *expire-time* 86400)
-(def *version* 0)
-(def *config-id* "b2bcomm-daily")
+(def default-expire-time 86400)
 
-(def *tair* (mk-tair *config-id*))
-(.init *tair*)
+(def tnamespace (atom 89))
+(def version (atom 0))
+(def config-id (atom "b2bcomm-daily"))
+
+(def tair (atom (mk-tair @config-id)))
+(.init @tair)
+
+(defn set-namespace
+  "Sets the tair namespace you want to operate in."
+  [ns]
+  (reset! tnamespace ns))
+
+(defn set-config-id
+  [new-config-id]
+  (reset! config-id new-config-id)
+  (reset! tair (mk-tair @config-id))
+  (.init @tair))
 
 (defn query
-  "Query the specified key in the namespace"
+  "Query the specified key in the @tnamespace"
   [key]
-  (let [obj (.get *tair* *namespace* key)]
+  (let [obj (.get @tair @tnamespace key)]
     obj))
 
 (defn put
-  "put something into tair"
-  [key value]
-  (.put *tair* *namespace* key value *version* *expire-time*))
+  "put something into @tair"
+  ([key value]
+     (put key value default-expire-time))
+  ([key value expire-time]
+     (.put @tair @tnamespace key value @version expire-time)))
 
 (defn delete [key]
-  "Delete something from tair."
-  (.delete *tair* *namespace* key))
+  "Delete something from @tair."
+  (.delete @tair @tnamespace key))
