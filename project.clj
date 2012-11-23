1176cb0ff1b63fa36f69ad8f41f690029676fac3
James Xu <xumingming64398966@gmail.com>
2012-11-23 13:27:32 +0800

    add fastjson deps

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject tair-repl "1.0.0-SNAPSHOT"
   :description "Tair Repl"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.taobao.tair/tair-mc-client "1.0.4.7"]])
+                 [com.taobao.tair/tair-mc-client "1.0.4.7"]
+                 [com.alibaba.platform.shared/fastjson "1.1.10"]
+                 [dynapath "0.2.0"]])
