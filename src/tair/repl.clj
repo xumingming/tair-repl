82c4f7aabb2f40552d4a932fb6b5d37b281d2838
James Xu <xumingming64398966@gmail.com>
2013-01-08 18:35:44 +0800

    add more checking

--- src/tair/repl.clj
+++ src/tair/repl.clj
@@ -99,29 +99,35 @@
                             (set-namespace new-namespace)
                             (println "namespace set to " new-namespace))
         
-          "put"   (let [key (first argv)
-                        value (second argv)
-                        value-type (if (> (count argv) 2)
-                                     (nth argv 2)
-                                     nil)
-                        value (condp = value-type
-                                "int" (Integer/valueOf value)
-                                "long" (Long/valueOf value)
-                                value)
-                        result-code (put @tair @tnamespace key value)]
-                    (if (= (:code result-code) 0)
-                      (println "SUCCESS!")
-                      (println "FAIL! code:" (:code result-code) ", message:" (:message result-code))))
+          "put"   (if (< (count argv) 2)
+                    (println "put expects at least 2 args," (count argv) "given.")
+                    (let [key (first argv)
+                          value (second argv)
+                          value-type (if (> (count argv) 2)
+                                       (nth argv 2)
+                                       nil)
+                          value (condp = value-type
+                                  "int" (Integer/valueOf value)
+                                  "long" (Long/valueOf value)
+                                  value)
+                          result-code (put @tair @tnamespace key value)]
+                      (if (= (:code result-code) 0)
+                        (println "SUCCESS!")
+                        (println "FAIL! code:" (:code result-code) ", message:" (:message result-code)))))
         
-          "get" (let [key (first argv)
-                      ret (get @tair @tnamespace key)]
-                  (pprint/pprint ret))
+          "get" (if (not= (count argv) 1)
+                  (println "get expects 1 arg," (count argv) "given.")
+                  (let [key (first argv)
+                        ret (get @tair @tnamespace key)]
+                    (pprint/pprint ret)))
         
-          "delete" (let [key (first argv)
-                         result-code (delete @tair @tnamespace key)]
-                     (if (= (:code result-code) 0)
-                       (println "SUCCESS!")
-                       (println "FAIL! code:" (:code result-code) ", message:" (:message result-code))))
+          "delete" (if (not= (count argv) 1)
+                     (println "delete expects 1 arg," (count argv) "given.")
+                     (let [key (first argv)
+                           result-code (delete @tair @tnamespace key)]
+                       (if (= (:code result-code) 0)
+                         (println "SUCCESS!")
+                         (println "FAIL! code:" (:code result-code) ", message:" (:message result-code)))))
         
           "settings"    (env)
 
